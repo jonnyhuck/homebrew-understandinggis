a042ba450975cb4d74db955333da59436b48c3ff
Jonny Huck <jonnyhuck@gmail.com>
2018-06-01 13:10:38 +0100

    Update mapnik2.rb

--- Formula/mapnik2.rb
+++ Formula/mapnik2.rb
@@ -53,6 +53,7 @@ class Mapnik2 < Formula
     jobs = ENV.make_jobs.to_i
     jobs /= 2 if jobs > 2
 
+    #"BOOST_LIBS=#{boost}/lib",
     args = ["CC=\"#{ENV.cc}\"",
             "CXX=\"#{ENV.cxx}\"",
             "JOBS=#{jobs}",
@@ -67,7 +68,6 @@ class Mapnik2 < Formula
             "TIFF_INCLUDES=#{libtiff}/include",
             "TIFF_LIBS=#{libtiff}/lib",
             "BOOST_INCLUDES=#{boost}/include",
-            #"BOOST_LIBS=#{boost}/lib",
             "BOOST_LIBS=/usr/local/opt/boost@1.59/lib",
             "PROJ_INCLUDES=#{proj}/include",
             "PROJ_LIBS=#{proj}/lib",
